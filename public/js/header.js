f1117c660848b6178cf15c080775492c95013200
EMMANUELSCH19 <emmanuel_1320114041@uptecamac.edu.mx>
2022-04-07 22:11:03 -0500

    Primera Subida

--- public/js/header.js
+++ public/js/header.js
@@ -1,10 +1,7 @@
 document.getElementById("btn-menu").addEventListener("click", open_close_menu);
 
-
-
 const menu = document.getElementById("menu");
 const btnMenu = document.getElementById("btn-menu");
-const ancla = document.getElementsById("ancla");
 
 function open_close_menu(){
     btnMenu.classList.toggle('active');
